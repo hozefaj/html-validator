8dff270bcbf5078480c63ca16e79ef4f43c0abbd
hozefaj <hozefarules@gmail.com>
2015-01-28 19:11:35 -0800

    ability to run on any webpage

--- event.js
+++ event.js
@@ -2,7 +2,10 @@
 // http://philipwalton.com/articles/introducing-html-inspector
 // https://cdnjs.cloudflare.com/ajax/libs/html-inspector/0.8.1/html-inspector.js
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript({
-        code: 'HTMLInspector.inspect()'
+
+    chrome.tabs.executeScript(tab.id, {file: "inspector.js"}, function(){
+        chrome.tabs.executeScript({
+            code: 'HTMLInspector.inspect()'
+        });
     });
 });
